2173f1b4e22a1a4b2ace7e92528ee1897a477e24
Abbu2019 <akber86@gmail.com>
2019-04-03 16:09:43 +0300

    Add files via upload

---
+++ Maximum No Tracer.py
@@ -0,0 +1,9 @@
+
+x=int(input("enter first no"))
+y=int(input("enter second no"))
+z=int(input("enter third no"))
+
+print("Max of three values is :")
+print (max(x,y,z))
+
+input("please hit enter to exit")
